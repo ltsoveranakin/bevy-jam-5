ab504959c66349b34c06c3aaa779ba2fdccdecce
ltsoveranakin <109498476+ltsoveranakin@users.noreply.github.com>
2024-07-25 15:24:52 -0400

    implement jumping

--- src/player.rs
+++ src/player.rs
@@ -2,32 +2,43 @@ use bevy::prelude::*;
 use bevy_rapier2d::prelude::*;
 
 const PLAYER_SPEED: f32 = 0.5;
-const JUMP_POWER: f32 = 50.;
+const JUMP_POWER: f32 = 1.;
+const PLAYER_GRAVITY: f32 = 10.;
+const PLAYER_MAX_GRAVITY: f32 = 100.;
 
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_player)
-            .add_systems(Update, move_player);
+        app.add_systems(Startup, spawn_player).add_systems(
+            Update,
+            (
+                player_gravity,
+                move_player.after(player_gravity),
+                apply_player_velocity.after(move_player),
+            ),
+        );
     }
 }
 
 #[derive(Component)]
 pub struct Player;
 
+#[derive(Component)]
+struct PlayerVelocity(Vec2);
+
 fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands
         .spawn((
             Player,
+            PlayerVelocity(default()),
             InheritedVisibility::default(),
             Collider::ball(16.),
-            RigidBody::Dynamic,
+            RigidBody::KinematicVelocityBased,
             KinematicCharacterController::default(),
+            KinematicCharacterControllerOutput::default(),
             TransformBundle::default(),
-            GravityScale::default(),
             LockedAxes::ROTATION_LOCKED,
-            Velocity::default(),
             Ccd::enabled(),
         ))
         .with_children(|parent| {
@@ -39,28 +50,53 @@ fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>) {
 }
 
 fn move_player(
-    mut player_query: Query<(&mut KinematicCharacterController, &mut Velocity)>,
+    mut player_query: Query<(&mut PlayerVelocity, &KinematicCharacterControllerOutput)>,
     keys: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>,
 ) {
-    let (mut character_controller, mut velocity) = player_query.single_mut();
+    let (mut player_velocity, controller_output) = player_query.single_mut();
 
-    let mut translation = Vec2::ZERO;
+    let mut translation = 0.;
 
-    if keys.pressed(KeyCode::KeyW) {
-        velocity.linvel.y = JUMP_POWER;
+    if controller_output.grounded && (keys.pressed(KeyCode::KeyW) || keys.pressed(KeyCode::Space)) {
+        player_velocity.0.y = JUMP_POWER;
     }
 
     if keys.pressed(KeyCode::KeyA) {
-        translation.x -= PLAYER_SPEED;
+        translation -= PLAYER_SPEED;
     }
 
     if keys.pressed(KeyCode::KeyD) {
-        translation.x += PLAYER_SPEED;
+        translation += PLAYER_SPEED;
     }
 
-    character_controller.translation = if translation == Vec2::ZERO {
-        None
+    player_velocity.0.x = translation;
+}
+
+fn player_gravity(
+    mut player_query: Query<(&mut PlayerVelocity, &KinematicCharacterControllerOutput)>,
+    time: Res<Time>,
+) {
+    let (mut player_velocity, controller_output) = player_query.single_mut();
+
+    if !controller_output.grounded {
+        player_velocity.0.y -= PLAYER_GRAVITY * time.delta_seconds();
+
+        if player_velocity.0.y < -PLAYER_MAX_GRAVITY {
+            player_velocity.0.y = -PLAYER_MAX_GRAVITY;
+        }
     } else {
-        Some(translation)
-    };
+        player_velocity.0.y = 0.;
+    }
+}
+
+fn apply_player_velocity(
+    mut player_query: Query<(&mut KinematicCharacterController, &PlayerVelocity)>,
+) {
+    let (mut character_controller, player_velocity) = player_query.single_mut();
+
+    println!("{}", player_velocity.0);
+    if player_velocity.0 != Vec2::ZERO {
+        character_controller.translation = Some(player_velocity.0);
+    }
 }
