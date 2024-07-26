79ffdee288dca7b607eb53e2cc1f8c76cdf6fae3
ltsoveranakin <109498476+ltsoveranakin@users.noreply.github.com>
2024-07-25 23:27:46 -0400

    use more convenient notation for level creation

--- src/levels/data.rs
+++ src/levels/data.rs
@@ -1,15 +1,50 @@
+use std::fmt;
 use std::fmt::{Display, Formatter};
 
 use bevy::asset::Asset;
 use bevy::math::UVec2;
 use bevy::prelude::TypePath;
 use bevy_ecs_tilemap::tiles::TilePos;
-use serde::Deserialize;
+use serde::de;
+use serde::de::Visitor;
+use serde::{Deserialize, Deserializer};
 
-#[derive(Deserialize, Debug, Copy, Clone, Hash, Eq, PartialEq)]
+#[derive(Debug, Copy, Clone, Hash, Eq, PartialEq)]
 pub struct LocationData {
     pub x: u32,
     pub y: u32,
+}
+
+struct LocationVisitor;
+
+impl<'de> Visitor<'de> for LocationVisitor {
+    type Value = LocationData;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("a string with two comma separated integers with the format x,y")
+    }
+
+    fn visit_str<E>(self, value: &str) -> Result<LocationData, E>
+    where
+        E: de::Error,
+    {
+        let parts: Vec<&str> = value.split(',').collect();
+        if parts.len() != 2 {
+            return Err(de::Error::invalid_value(de::Unexpected::Str(value), &self));
+        }
+        let x = parts[0].parse::<u32>().map_err(de::Error::custom)?;
+        let y = parts[1].parse::<u32>().map_err(de::Error::custom)?;
+        Ok(LocationData { x, y })
+    }
+}
+
+impl<'de> Deserialize<'de> for LocationData {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_string(LocationVisitor)
+    }
 }
 
 impl Display for LocationData {
